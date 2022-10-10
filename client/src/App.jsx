369a9e27f0ccbef3477c252da180364e1d2d008b
bayramyavuzcicek <cicekbayramyavuz@gmail.com>
2022-10-10 09:50:30 +0300

    header tasarımı yapıldı

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
-import TopBar from "./components/TopBar/TopBar";
 import './app.css'
+import TopBar from "./components/TopBar/TopBar";
+import Home from "./components/Pages/Home/Home";
+
 function App() {
   return (
     <div className="app">
       <TopBar/>
-      
+      <Home/>
     </div>
   );
 }
