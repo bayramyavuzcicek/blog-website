13b72e61d0a3a3b327f49cf474bf6d2700e33129
bayramyavuzcicek <cicekbayramyavuz@gmail.com>
2022-10-10 13:51:03 +0300

    settings page tasarımı yapıldı

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,13 @@ import TopBar from "./components/topBar/TopBar";
 import Home from "./components/Pages/home/Home";
 import Single from './components/Pages/single/Single';
 import Write from './components/Pages/write/Write';
+import Settings from './components/Pages/settings/Settings';
 
 function App() {
   return (
     <div className="app">
       <TopBar/>
-      <Write/>
+      <Settings/>
     </div>
   );
 }
